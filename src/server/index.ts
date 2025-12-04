341564e0ec0e75cd2c01763bf98e7e0b6c33c446
MunBrian <bryanmungai254@gmail.com>
2025-12-04 23:16:52 +0300

    feat: add production and staging env in wrangler file. Add middleware to authenticate user access to public worker url

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from "hono";
+import { accessAuth } from "./middleware/auth";
 const app = new Hono();
 
-app.get("/api/health", (c) => c.json('Healthy 👌👌'));
+app
+.use(accessAuth)
+.get("/api/health", (c) => c.json('Healthy 👌👌'));
 
 export default app;
