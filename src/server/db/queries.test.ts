037bd89a558094934f053f1adffd6c15f8d47f10
MunBrian <bryanmungai254@gmail.com>
2025-12-16 23:15:41 +0300

    feat: add test to check for duplicate emails

--- src/server/db/queries.test.ts
+++ src/server/db/queries.test.ts
@@ -1,14 +1,23 @@
 import {it, expect, mock, beforeEach} from "bun:test"
 import { insertSubscriber } from "./queries"
 import type { D1Database } from "@cloudflare/workers-types"
-import type { NewSubscriber } from "./schema"
+import type {NewSubscriber } from "./schema"
+import * as schema from "./schema"
 import { getTestDB } from "../../../test/get-test-db"
-
+import {reset} from "drizzle-seed"
 //replace getDb function in db.ts with getTestDB function within the test folder
 mock.module("./db.ts", () => {
     return {
         getDB: () => getTestDB()
     }
+})
+
+//clear data in the test db before running the tests
+beforeEach(async() => {
+    const db = getTestDB();
+
+    //reset db
+    await reset(db, schema)
 })
 
 it("insert a new subscriber", async () => {
@@ -20,10 +29,23 @@ it("insert a new subscriber", async () => {
 
     const subscriber = await insertSubscriber({} as D1Database, newSub )
 
+     console.log(subscriber)
+
     expect(subscriber.email).toBe(newSub.email)
     expect(subscriber.id).toBeDefined()
     expect(subscriber.createdAt).toBeDefined()
 
-    console.log(subscriber)
 
+})
+
+
+it("throws an error when inserting duplicate emails to db", async() => {
+    //create a subscriber
+    const newSub: NewSubscriber = {
+        email: 'test@test.com' 
+    }
+
+    await insertSubscriber({} as D1Database, newSub )
+
+    expect(insertSubscriber({} as D1Database, newSub )).rejects.toThrow()
 })
